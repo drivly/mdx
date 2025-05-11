2117b16452312a9e64e023b83b24609b8c40032d
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-05-11 06:07:59 -0500

    refactor(mdxdb): updated mdx labels

--- packages/mdxdb/src/collections/MDX.ts
+++ packages/mdxdb/src/collections/MDX.ts
@@ -1,15 +1,22 @@
 import type { CollectionConfig } from 'payload'
 import { editorOptions } from '@/lib/collections'
 
+const defaultValue = `---
+
+---
+
+# {name}
+`
+
 export const MDX: CollectionConfig = {
   slug: 'mdx',
   labels: { singular: 'MDX', plural: 'MDX' },
   fields: [
     { type: 'row', fields: [
       { name: 'folder', type: 'relationship', relationTo: 'folders', required: true },
-      { name: 'id', type: 'text', required: true },
+      { name: 'id', type: 'text', required: true, label: 'ID' },
     ] },
-    { name: 'content', type: 'code', admin: { language: 'mdx', editorOptions } },
+    { name: 'content', type: 'code', defaultValue, admin: { language: 'mdx', editorOptions } },
     { name: 'data', type: 'json', admin: { editorOptions, readOnly: true } },
   ],
 }
